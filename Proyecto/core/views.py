b702a02a83f7e9c7ecb378b126f47890e8d2f04b
Dani-2206 <dlandero.alexander@gmail.com>
2023-10-09 13:57:16 -0300

    se ha implementado el filtrador

--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -52,19 +52,27 @@ def imperiohelenica(request):
 #ver los productos ingresados
 @user_passes_test(is_admin, login_url='index')
 def ver(request):
-    queryset = request.GET.get("buscar")
-    print(queryset)
     producto=Producto.objects.all()
+    categorias = Categoria.objects.all()
+    queryset = request.GET.get("buscar")
+    categoria = request.GET.get("categoria")
+    precio_min = request.GET.get("precio_min")
+    precio_max = request.GET.get("precio_max")
+    print(queryset)
+
     if queryset:
         producto = Producto.objects.filter(
             Q(nombre_P__icontains = queryset) |
             Q(descripcion__icontains= queryset)
+        ).distinct()
 
-        ).distinct
-    
-    
-    
-    return render(request,'CRUD/CRUD.html',{'producto':producto})
+    if categoria:
+        producto = producto.filter(id_Categoria=categoria)
+
+    if precio_min and precio_max:
+        producto = producto.filter(precio__range=(precio_min,precio_max))
+
+    return render(request,'CRUD/CRUD.html',{'producto':producto,'categorias':categorias})
 
 @user_passes_test(is_admin, login_url='index')
 #agregar
