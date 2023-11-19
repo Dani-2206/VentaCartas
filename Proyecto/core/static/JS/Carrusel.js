d5f4afbf2e84602d725ff5aeb093a076b32b1c9a
unknown <nico.prieto@duocuc.cl>
2023-11-19 18:36:21 -0300

    se ha agregado cosas al crud

--- Proyecto/core/static/JS/Carrusel.js
+++ Proyecto/core/static/JS/Carrusel.js
@@ -1,36 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    coverflowEffect: {
-      depth: 300,
-      spaceBetween: 10
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    breakpoints: {
-      // Mostrar 2 diapositivas a la vez en dispositivos móviles
-      640: {
-        slidesPerView: 2
-      }
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets"
-    },
-    slidesPerView: 1, // Mostrar 2 diapositivas a la vez
-    minslidesPerView: 1 // Mostrar al menos 1 diapositiva a la vez
-})
-var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 1000, // Cambia cada 3 segundos
-}})
+$('.carousel[data-type="multi"] .item').each(function() {
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < 2; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
